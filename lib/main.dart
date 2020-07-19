fbfe836d5161c094b30c783916c92d2e2c491a4a
adriano <adrianorodrigues38@gmail.com>
2020-07-19 18:43:43 -0300

    Commit ! ADRIANO Esteve Aqui!!!!!!!!!!!!!!!!

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Adriano Esteve Aqui!!!!!!'),
     );
   }
 }
@@ -98,7 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'Olá ele estev aqui hahahahaha:',
             ),
             Text(
               '$_counter',
