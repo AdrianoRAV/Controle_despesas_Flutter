ff8d4a9b5de6ff5037f5f3c0f14a2f5b4fded635
adriano <adrianorodrigues38@gmail.com>
2020-07-20 10:16:42 -0300

    commit da primeira parte

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:appcontroledespesas/graph_widget.dart';
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() => runApp(MyApp());
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -10,25 +13,41 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: HomePage(),
     );
   }
 }
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-class _MyHomePageState extends State<MyHomePage> {
 
-  Widget _bottomAction(IconData icon){
-    return InkWell (
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  PageController _controller;
+  int currentPage = 9;
+
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = PageController(
+      initialPage: currentPage,
+      viewportFraction: 0.4,
+    );
+  }
+
+  Widget _bottomAction(IconData icon) {
+    return InkWell(
       child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Icon(icon),
       ),
-      onTap: (){},
+      onTap: () {},
     );
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,9 +56,9 @@ class _MyHomePageState extends State<MyHomePage> {
         shape: CircularNotchedRectangle(),
         child: Row(
           mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,//alinhamento dos icones
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-          _bottomAction(FontAwesomeIcons.history),//icones
+            _bottomAction(FontAwesomeIcons.history),
             _bottomAction(FontAwesomeIcons.chartPie),
             SizedBox(width: 48.0),
             _bottomAction(FontAwesomeIcons.wallet),
@@ -50,46 +69,160 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: (){},
+        onPressed: () {},
       ),
       body: _body(),
     );
-}
-  Widget _body(){
+  }
+
+  Widget _body() {
     return SafeArea(
       child: Column(
         children: <Widget>[
           _selector(),
           _expenses(),
           _graph(),
+          Container(
+            color: Colors.blueAccent.withOpacity(0.15),
+            height: 24.0,
+          ),
           _list(),
         ],
       ),
     );
   }
-  Widget _selector() => Container();
+
+  Widget _pageItem(String name, int position) {
+    var _alignment;
+    final selected = TextStyle(
+      fontSize: 20.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.blueGrey,
+    );
+    final unselected = TextStyle(
+      fontSize: 20.0,
+      fontWeight: FontWeight.normal,
+      color: Colors.blueGrey.withOpacity(0.4),
+    );
+
+    if (position == currentPage) {
+      _alignment = Alignment.center;
+    } else if (position > currentPage) {
+      _alignment = Alignment.centerRight;
+    } else {
+      _alignment = Alignment.centerLeft;
+    }
+
+    return Align(
+      alignment: _alignment,
+      child: Text(name,
+        style: position == currentPage ? selected : unselected,
+      ),
+    );
+  }
+
+  Widget _selector() {
+    return SizedBox.fromSize(
+      size: Size.fromHeight(70.0),
+      child: PageView(
+        onPageChanged: (newPage) {
+          setState(() {
+            currentPage = newPage;
+          });
+        },
+        controller: _controller,
+        children: <Widget>[
+          _pageItem("Janeiro", 0),
+          _pageItem("Fevereiro", 1),
+          _pageItem("Março", 2),
+          _pageItem("Abril", 3),
+          _pageItem("Maio", 4),
+          _pageItem("Junho", 5),
+          _pageItem("Julho", 6),
+          _pageItem("Agosto", 7),
+          _pageItem("Setembro", 8),
+          _pageItem("Outubro", 9),
+          _pageItem("Novembro", 10),
+          _pageItem("Decembro", 11),
+        ],
+      ),
+    );
+  }
 
   Widget _expenses() {
     return Column(
       children: <Widget>[
         Text("\$2361,41",
-        style: TextStyle(
-          fontWeight: FontWeight.bold,
-          fontSize: 40.0,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 40.0,
+          ),
         ),
+        Text("Total gastos",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 16.0,
+            color: Colors.blueGrey,
+          ),
         ),
-        Text("Total despesas",
-        style: TextStyle(
-          fontWeight: FontWeight.bold,
-          fontSize: 46.0,
-          color: Colors.blueGrey,
-        ),
-        )
-      ]
+      ],
     );
   }
-  Widget _graph() => Container();
 
-  Widget _list() => Container();
+  Widget _graph() {
+    return Container(
+      height: 250.0,
+      child: GraphWidget(),
+    );
+  }
 
+  Widget _item(IconData icon, String name, int percent, double value) {
+    return ListTile(
+      leading: Icon(icon, size: 32.0,),
+      title: Text(name,
+        style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20.0
+        ),
+      ),
+      subtitle: Text("$percent% de gastos",
+        style: TextStyle(
+          fontSize: 16.0,
+          color: Colors.blueGrey,
+        ),
+      ),
+      trailing: Container(
+        decoration: BoxDecoration(
+          color: Colors.blueAccent.withOpacity(0.2),
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text("\$$value",
+            style: TextStyle(
+              color: Colors.blueAccent,
+              fontWeight: FontWeight.w500,
+              fontSize: 16.0,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _list() {
+    return Expanded(
+      child: ListView.separated(
+        itemCount: 15,
+        itemBuilder: (BuildContext context, int index) =>
+            _item(FontAwesomeIcons.shoppingCart, "Shopping", 14, 145.12),
+        separatorBuilder: (BuildContext context, int index) {
+          return Container(
+            color: Colors.blueAccent.withOpacity(0.15),
+            height: 8.0,
+          );
+        },
+      ),
+    );
+  }
 }
